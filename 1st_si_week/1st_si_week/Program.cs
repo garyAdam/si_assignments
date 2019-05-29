0b4b66f5be8f3fdaa01663af44fc28c806f1361b
Gergő Ádám <rapture047@gmail.com>
2019-05-29 16:32:54 +0200

    1st Excercise done

--- 1st_si_week/1st_si_week/Program.cs
+++ 1st_si_week/1st_si_week/Program.cs
@@ -7,6 +7,43 @@ namespace _1st_si_week
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Person person = new Person();
+            person.BirthDate = 1998;
+            person.Name = "Johnnyy";
+            Console.WriteLine(person.ToString());
         }
+    }
+
+    class Person
+    {
+        string name;
+        int birthDate;
+
+        public int BirthDate
+        {
+            get { return birthDate; }
+            set
+            {
+                birthDate = value;
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+
+        }
+        public override string ToString()
+        {
+            return "Name: " + name + ", BirthDate: " + birthDate;
+            
+        }
+
+
+
+    }
+    enum Genders
+    {
+        Male, Female
     }
 }
