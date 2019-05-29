0dc0380e1e08cd2640ad35f66ce9cc4fe52ab6f3
Gergő Ádám <rapture047@gmail.com>
2019-05-29 15:56:03 +0200

    init

---
+++ 1st_si_week/1st_si_week/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1st_si_week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
