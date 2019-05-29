d1c90e905afe289cda55691e191f54ffc12a1a5e
Gergő Ádám <rapture047@gmail.com>
2019-05-29 16:47:33 +0200

    Excercise 2 done

--- 1st_si_week/1st_si_week/Program.cs
+++ 1st_si_week/1st_si_week/Program.cs
@@ -11,13 +11,23 @@ namespace _1st_si_week
             person.BirthDate = 1998;
             person.Name = "Johnnyy";
             Console.WriteLine(person.ToString());
+
+            Employee employee = new Employee();
+            employee.Name = "jozsi";
+            employee.BirthDate = 1989;
+            employee.Profession = "vizes";
+            employee.Salary = 100000;
+            Room room1 = new Room();
+            room1.RoomNumber = 1;
+            employee.Room = room1;
+            Console.WriteLine( employee.ToString());
         }
     }
 
     class Person
     {
-        string name;
-        int birthDate;
+        protected string name;
+        protected int birthDate;
 
         public int BirthDate
         {
@@ -36,12 +46,36 @@ namespace _1st_si_week
         public override string ToString()
         {
             return "Name: " + name + ", BirthDate: " + birthDate;
-            
+
         }
 
 
 
     }
+    class Employee : Person
+    {
+        private int salary;
+        private string profession;
+        private Room room;
+
+
+        public int Salary { get => salary; set => salary = value; }
+        public string Profession { get => profession; set => profession = value; }
+
+        public override string ToString()
+        {
+            return "Name: " + name + ", BirthDate: " + birthDate + ", Salary: " + salary + ", Profession: " + profession + ", Room Number: "+Room.RoomNumber;
+        }
+        public Room Room { get => room; set => room = value; }
+    }
+
+    class Room
+    {
+        int roomNumber;
+
+        public int RoomNumber { get => roomNumber; set => roomNumber = value; }
+    }
+
     enum Genders
     {
         Male, Female
