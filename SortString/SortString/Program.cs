1f8313d98a9d69fbeb9ab8d1cd08b114ee8ef3d9
Gergő Ádám <rapture047@gmail.com>
2019-05-30 16:15:19 +0200

    SortString done

---
+++ SortString/SortString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Testing the string tester testing test";
+            string[] testArray = test.Split(" ");
+            Array.Sort(testArray);
+
+            test = string.Join(" ", testArray);
+            Console.WriteLine(test);
+        }
+    }
+}
